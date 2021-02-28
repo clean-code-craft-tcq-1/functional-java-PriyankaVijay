c742a88a1a1fcfa3ef0b24baddf76a7b42002282
PriyankaVijay <priyankapinky345@gmail.com>
2021-02-28 21:17:28 +0530

    Updated Assignment

---
+++ vitals/Battery.java
@@ -0,0 +1,46 @@
+package vitals;
+
+public class Battery {
+	float temperature;
+	float soc;
+	float chargeRate;
+
+	public Battery(float temperature, float soc, float chargeRate) {
+		this.temperature = temperature;
+		this.soc = soc;
+		this.chargeRate = chargeRate;
+	}
+	public boolean isBatteryOk() {
+		return (isTemperatureOk() && isSocOk() && isChargeRateOk()) ? true : false;
+	}
+	public boolean isTemperatureOk() {		
+		if(!isFactorOk(Constants.TEMPERATURE_MIN_THRESHOLD, Constants.TEMPERATURE_MAX_THRESHOLD, temperature))
+		{
+			System.out.println("Temperature is out of range and is " + isFactorHigh(Constants.TEMPERATURE_MAX_THRESHOLD , temperature) + "!");
+			return false;
+		}	
+		return true;
+	}
+	public boolean isSocOk() {
+		if(!isFactorOk(Constants.SOC_MIN_THRESHOLD, Constants.SOC_MAX_THRESHOLD, soc))
+		{
+			System.out.println("State of charge is out of range and is " + isFactorHigh(Constants.TEMPERATURE_MAX_THRESHOLD , soc) + "!");
+			return false;
+		}	
+		return true;
+	}
+	public boolean isChargeRateOk() {
+		if(isFactorHigh(Constants.CHARGE_RATE_MAX_THRESHOLD, chargeRate).equals("high"))
+		{
+			System.out.println("Charge Rate is out of range!");
+            return false;
+		}
+		return true;
+	}
+	private String isFactorHigh(float max, float factor) {
+		return (factor>max) ? "high" : "low"; 
+	}
+	private boolean isFactorOk(float min, float max, float value) {
+		return (value < min || value > max) ? false : true; 
+	}
+}
